ae8a6b9c867863a5e2c5edd12da4ea0c59297487
MAHRifat <hasanrifata2@gmail.com>
2024-07-11 15:27:34 +0600

    Handling Deletion

--- Backend/Relationship Mongoosh/Models/user.js
+++ Backend/Relationship Mongoosh/Models/user.js
@@ -10,19 +10,19 @@ async function main() {
 // one to many
     // one to few
 
-    const userSchema = new Schema({
-        username: String,
-        address: [
-            {
-                _id: false,         // use for don't want any id for the addresses
-                location: String,
-                city: String,
-            },
-        ]
-    });
+    // const userSchema = new Schema({
+    //     username: String,
+    //     address: [
+    //         {
+    //             _id: false,         // use for don't want any id for the addresses
+    //             location: String,
+    //             city: String,
+    //         },
+    //     ]
+    // });
 
 
-    const User = mongoose.model("User", userSchema);
+    // const User = mongoose.model("User", userSchema);
 
     // const addUsers = async()=> {
     //     let user1 = new User({
@@ -50,35 +50,53 @@ async function main() {
         item: String,
         price: Number,
     });
-
-    const Order = mongoose.model("Order", orderSchema);
-
-    // const addOrder = async()=> {
-    //    let res = await Order.insertMany([ 
-    //         {item: "Somasa", price: 10},
-    //         {item: "Singara", price: 10},
-    //         {item: "Vegi-Role", price: 20},
-    //         {item: "Chicken_Role", price:30}
-    //     ]);
-    //     console.log(res);
-    // }
-
-    // addOrder();
-
-    const customerSchema = new Schema({
-        name: String,
-        orders: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Order"
-            }
-        ]
-    });
     
+    // const addOrder = async()=> {
+        //    let res = await Order.insertMany([ 
+            //         {item: "Somasa", price: 10},
+            //         {item: "Singara", price: 10},
+            //         {item: "Vegi-Role", price: 20},
+            //         {item: "Chicken_Role", price:30}
+            //     ]);
+            //     console.log(res);
+            // }
+            
+            // addOrder();
+            
+            const customerSchema = new Schema({
+                name: String,
+                orders: [
+                    {
+                        type: Schema.Types.ObjectId,
+                        ref: "Order"
+                    }
+                ]
+            });
 
-    const Customer = mongoose.model("Customer", customerSchema);
+    // Delete handleing middleware
+        // pre  -> run before delete
 
-    const addCustomer = async()=>{
+            // customerSchema.pre("findOneAndDelete", async()=>{
+            //     console.log("per middleware");
+            // });
+
+
+        // post -> run after delete 
+            // we use post middleware for delete orders after delete the customer
+
+            customerSchema.post("findOneAndDelete", async(customer)=> {
+                if(customer.orders.length){
+                    let res = await Order.deleteMany({_id: { $in: customer.orders}});
+                    console.log(res);
+                }
+            });
+        
+            
+            
+            const Order = mongoose.model("Order", orderSchema);
+            const Customer = mongoose.model("Customer", customerSchema);
+
+    // const addCustomer = async()=>{
         // let cust1 = new Customer({
         //     name: "Rifat",
         // });
@@ -95,32 +113,32 @@ async function main() {
         // let result = await Customer.find({});    // by default only id will store in the mongoose although we push the hole information
         // console.log(result);
         // let resPopulate = await Customer.find({}).populate("orders");
-        // console.log(resPopulate[0]);  // it will show the hote detailes of order info
-    }
+        // console.log(resPopulate[0]);  // it will show the hole detailes of order info
+    // }
 
-    addCustomer();
+    // addCustomer();
 
 
 
     // One to many / Approach 3 (one to squilliones)  
         // -> Store a reference to the parent document inside child
 
-    const UserSchema = new Schema({
-        username: String,
-        email: String,
-    });
+    // const UserSchema = new Schema({
+    //     username: String,
+    //     email: String,
+    // });
 
-    const InstUser = mongoose.model("InstUser", UserSchema);
+    // const InstUser = mongoose.model("InstUser", UserSchema);
 
-    const PostSchema = new Schema({
-        content: String,
-        likes: Number,
-        user: {
-            type: Schema.Types.ObjectId, ref: "InstUser"
-        },
-    });
+    // const PostSchema = new Schema({
+    //     content: String,
+    //     likes: Number,
+    //     user: {
+    //         type: Schema.Types.ObjectId, ref: "InstUser"
+    //     },
+    // });
 
-    const Post = mongoose.model("Post", PostSchema);
+    // const Post = mongoose.model("Post", PostSchema);
 
     // const addData = async()=>{
         // let user1 = new InstUser({     // insert user1 to mongoosh
@@ -145,12 +163,12 @@ async function main() {
 
     // addData();
 
-    const getData = async()=>{
-        let res = await Post.find({}).populate("user");
-        console.log(res);
-    }
+    // const getData = async()=>{
+    //     let res = await Post.find({}).populate("user");
+    //     console.log(res);
+    // }
 
-    getData();
+    // getData();
 
 
 
@@ -159,3 +177,46 @@ async function main() {
 // Must read
     
     // https://www.mongodb.com/blog/post/6-rules-of-thumb-for-mongodb-schema-design
+
+
+
+
+// Handling Deletin -> using Mongoose Middleware
+    // we can use 2 middleware
+        // a. pre -> run before the query in executed
+        // b. post -> run after the query is executed
+
+    // the per and post middleware must be declare before the model
+
+    // we use 2 approch for handling deleting
+
+    const addCust = async ()=> {
+        let newCust = new Customer({
+            name: "Karan Arjun"
+        });
+
+        let newOrder = new Order({
+            item: "Teheri",
+            price: 180
+        });
+
+        newCust.orders.push(newOrder);
+
+        await newOrder.save();
+        await newCust.save();
+    }
+
+    // addCust();
+
+
+    // use for when we want to delete the customer not the order details
+
+    const deleteCust = async()=> {
+        let data = await Customer.findOneAndDelete({_id: '668fa40768227cf0c00346c7'});
+        console.log(data);
+    }
+
+    deleteCust();
+
+
+   
