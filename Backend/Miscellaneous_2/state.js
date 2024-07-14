8a7088a4ae0231b3ff1022c497273b3981eb7b12
MAHRifat <hasanrifata2@gmail.com>
2024-07-14 22:51:45 +0600

    Topic flash which help us show message or pop up message

--- Backend/Miscellaneous_2/state.js
+++ Backend/Miscellaneous_2/state.js
@@ -12,8 +12,14 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require('express-session');
+const flash = require('connect-flash');    
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(session({ secret: "mysupersecrets", resave: false, saveUninitialized: true }));
+app.use(flash());    // to use flash it is compolsary to use session
 
 app.get("/test", (req, res) => {
     res.send("test successful");
@@ -39,14 +45,19 @@ app.get("/register", (req, res)=> {
     req.session.name = name;   // cerate a session variable
     console.log(req.session.name);
     // res.send(name);
+    req.flash("success", "User registered successfully");    // show a pop up message before redirect to /hello
     res.redirect("/hello");
 });
 
 app.get("/hello",(req, res)=> {
-    res.send(`Hello, ${req.session.name}`);
+    // res.send(`Hello, ${req.session.name}`);
+    res.render("page.ejs", {name: req.session.name, msg: req.flash("success")});
 });
 
 
+// connect-flash -> npm i connect-flash
+    // The flash is a special areea of the sessions used for storing messages. Messages are written to the
+    // the flash and cleared after being displayed to the user.
 
 app.listen(3000, () => {
     console.log("Server is listening to 3000");
