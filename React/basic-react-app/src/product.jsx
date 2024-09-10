cc1d9191514720716fd4934c29639c4a464eec13
MAHRifat <hasanrifata2@gmail.com>
2024-09-11 00:17:13 +0600

    dynamic component styleing

--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -2,31 +2,12 @@ import './product.css';
 
 function Product({title, price = 0}){
 
-    // if(price >= 30000){
-    //     return(
-    //         <div className="Product">
-    //             <h3>{title}</h3>
-    //             <h5>{price}</h5>
-    //             <p>Discount: 5%</p>
-    //         </div>
-    //     );
-    // }
-    // else{
-    //     return(
-    //         <div className="Product">
-    //             <h3>{title}</h3>
-    //             <h5>{price}</h5>
-    //         </div>
-    //     );
-    // }
-
+    let styles = {backgroundColor: price > 30000 ? "blue" : null};
     return(
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <h5>{price}</h5>
-            {/* {price >= 30000 ? <p>"Discount: 5%"</p> : null} */}
-            {/* another way to write conditions */}
-            {price >= 30000 && <p>Discount: 5%</p>}  
+            {price >= 30000 ? <p>"Discount: 5%"</p> : null}
         </div>
     );
 
