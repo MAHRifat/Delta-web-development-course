7f0ea530dbdb5638dd5caa85c2f95dcedf567bad
MAHRifat <hasanrifata2@gmail.com>
2024-09-11 00:10:55 +0600

    conditions in jsx

--- React/basic-react-app/src/ProductCard.jsx
+++ React/basic-react-app/src/ProductCard.jsx
@@ -2,19 +2,12 @@ import { Product } from "./product";
 import './productCard.css';
 
 function ProductCard(){
-    // let features = [<li>Tech</li>, <li>waterproof</li> , <li>strong</li>, <li>Durable</li>];
-    let features = ["Tech", "waterproof", "strong", "Durable"];
-    let features2 = {
-        a: "hi-tech",
-        b: "waterproof",
-        c: "strong"
-    };
     return(
         <div className="productCard">
-            <Product title="Phone" price={30000} features={features} features2={features2}/>
-            <Product title="Laptop" price={60000} features={features} features2={features2}/>
-            <Product title={"Desktop"} price={"unlimited"} features={features} features2={features2}/>
-            <Product title={"Tab"} features={features} features2={features2}/>
+            <Product title="Phone" price={30000} />
+            <Product title="Laptop" price={60000} />
+            <Product title={"Desktop"} price={20000}/>
+            <Product title={"Tab"} />
         </div>
     );
 }
--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -1,24 +1,37 @@
 import './product.css';
 
-function Product({title, price = 0, features, features2}){
+function Product({title, price = 0}){
+
+    // if(price >= 30000){
+    //     return(
+    //         <div className="Product">
+    //             <h3>{title}</h3>
+    //             <h5>{price}</h5>
+    //             <p>Discount: 5%</p>
+    //         </div>
+    //     );
+    // }
+    // else{
+    //     return(
+    //         <div className="Product">
+    //             <h3>{title}</h3>
+    //             <h5>{price}</h5>
+    //         </div>
+    //     );
+    // }
+
     return(
         <div className="Product">
             <h3>{title}</h3>
-            <h4>{features.map((feature)=> <li>{feature}</li>)}</h4>
             <h5>{price}</h5>
-            <h3>{features2.b}</h3>
+            {/* {price >= 30000 ? <p>"Discount: 5%"</p> : null} */}
+            {/* another way to write conditions */}
+            {price >= 30000 && <p>Discount: 5%</p>}  
         </div>
     );
+
+  
 }
-// function Product(props){
-//     console.log(props);
-//     return(
-//         <div className="Product">
-//             <h3>{props.title}</h3>
-//             <h5>{props.price}</h5>
-//         </div>
-//     );
-// }
 
 
 export {Product};
