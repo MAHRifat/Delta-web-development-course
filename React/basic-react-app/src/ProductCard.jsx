db33c1611033ce94c0af9866ce4e09a6fcdfa649
MAHRifat <hasanrifata2@gmail.com>
2024-09-10 15:58:55 +0600

    passing arrays to props and rendering the array

--- React/basic-react-app/src/ProductCard.jsx
+++ React/basic-react-app/src/ProductCard.jsx
@@ -2,12 +2,19 @@ import { Product } from "./product";
 import './productCard.css';
 
 function ProductCard(){
+    // let features = [<li>Tech</li>, <li>waterproof</li> , <li>strong</li>, <li>Durable</li>];
+    let features = ["Tech", "waterproof", "strong", "Durable"];
+    let features2 = {
+        a: "hi-tech",
+        b: "waterproof",
+        c: "strong"
+    };
     return(
         <div className="productCard">
-            <Product title="Phone" price={30000}/>
-            <Product title="Laptop" price={60000}/>
-            <Product title={"Desktop"} price={"unlimited"}/>
-            <Product title={"Tab"}/>
+            <Product title="Phone" price={30000} features={features} features2={features2}/>
+            <Product title="Laptop" price={60000} features={features} features2={features2}/>
+            <Product title={"Desktop"} price={"unlimited"} features={features} features2={features2}/>
+            <Product title={"Tab"} features={features} features2={features2}/>
         </div>
     );
 }
--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -1,10 +1,12 @@
 import './product.css';
 
-function Product({title, price = 0}){
+function Product({title, price = 0, features, features2}){
     return(
         <div className="Product">
             <h3>{title}</h3>
+            <h4>{features.map((feature)=> <li>{feature}</li>)}</h4>
             <h5>{price}</h5>
+            <h3>{features2.b}</h3>
         </div>
     );
 }
