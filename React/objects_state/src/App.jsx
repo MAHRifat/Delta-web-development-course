326e37cc72bd1d465b9b0527d1e605dbc5093112
MAHRifat <hasanrifata2@gmail.com>
2024-09-15 00:38:14 +0600

    react form basic

--- React/objects_state/src/App.jsx
+++ React/objects_state/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Lottery from './Lottery'
 import LudoBoard from './LudoBoard'
 import ToDoList from './TodoList'
+import Form from './Form'
 function App() {
 
   return (
     <>
     {/* <LudoBoard/>        */}
     {/* <ToDoList/> */}
-    <Lottery/>
+    {/* <Lottery/> */}
+    <Form></Form>
     </>
   )
 }
