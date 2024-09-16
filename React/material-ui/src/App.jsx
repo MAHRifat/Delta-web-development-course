d653c969429f8f2598d13bdb8dcd54b32898b93f
MAHRifat <hasanrifata2@gmail.com>
2024-09-17 00:38:33 +0600

    learn material ui

---
+++ React/material-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return(
+    <>
+       <Button variant="contained">Click me!</Button>
+    </>
+  )
+}
+
+export default App
